5006eeccc0a0d52395276ad4c84705646ce6d6f1
falgunip02 <falguni.p0205@gmail.com>
2024-05-22 00:18:38 +0530

    initial

--- src/index.ts
+++ src/index.ts
@@ -3,62 +3,127 @@ import cors from "cors";
 import simpleGit from "simple-git";
 import { getAllFiles } from "./file";
 import { generate } from "./utils";
-import path = require("path");
-// import path from "path";
+import path from "path";
 import { uploadFile } from "./aws";
-//uploadFile("falguni/package.json","/Users/bhavi/Desktop/vercel/package.json");
 import { createClient } from "redis";
+
 const publisher = createClient();
-publisher.connect();
 
+async function connectRedis() {
+  try {
+    await publisher.connect();
+    console.log("Connected to Redis");
+  } catch (error) {
+    console.error("Failed to connect to Redis:", error);
+  }
+}
 
+connectRedis();
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-console.log(path.join(__dirname,`output/randomstring`))
+console.log(path.join(__dirname, `output/randomstring`));
 
-//POSTMAN
-app.post("/deploy",async (req,res) => {
-    const repoUrl = req.body.repoUrl;
-    const id = generate(); //asd12
-    await simpleGit().clone(repoUrl, path.join(__dirname,`output/${id}`));
+// POSTMAN
+app.post("/deploy", async (req, res) => {
+  const repoUrl = req.body.repoUrl;
+  const id = generate(); // asd12
 
-    const files = getAllFiles(path.join(__dirname,`output/${id}`));
+  try {
+    await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
+    console.log(`Cloned repository to: ${path.join(__dirname, `output/${id}`)}`);
 
-    files.forEach(async file => {
+    const files = getAllFiles(path.join(__dirname, `output/${id}`));
+    console.log(`Files to upload: ${files}`);
 
-        //from: //users/bhavi/vercel.....//output//13125512/app.js
-        //extract : //output/13125512/app.js
-        await uploadFile(file.slice(__dirname.length + 1),file);    //users/bhavi/vercel...../output/.. (yeh reemove ho jayega )
-    })
+    for (const file of files) {
+      const filePath = file.slice(__dirname.length + 1);
+      console.log(`Uploading file: ${filePath}`);
+      await uploadFile(filePath, file);
+    }
 
-    publisher.lPush("build-queue",id);
+    const result = await publisher.lPush("build-queue", id);
+    console.log(`Pushed to build-queue: ${result}`);
 
-    // console.log(files);
-    // files.forEach(file=>{
-    //     S3.upload
-    // })
-
-    //put this to s3
-    res.json({
-        id:id
-    })
+    res.json({ id: id });
+  } catch (error) {
+    console.error("Error during deploy:", error);
+    res.status(500).json({ error: "Deployment failed" });
+  }
 });
 
-    //aws-sdk
-    //sdk.uploadDirToS3()=> no easy way to call this
-    //need to make an array
-    // iterate thru this array one by array
-    //[/falz/users/project/vercel/output/12345/app.tsx]
-    //then call sdk.uploadFile one by one cz there isnt an easy way to upload sdk.folder
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 
 
-app.listen(3000);
 
-// function getAllFiles(arg0: any) {
-//     throw new Error("Function not implemented.");
-// }
+
+
+// import express from "express";
+// import cors from "cors";
+// import simpleGit from "simple-git";
+// import { getAllFiles } from "./file";
+// import { generate } from "./utils";
+// import path = require("path");
+// // import path from "path";
+// import { uploadFile } from "./aws";
+// //uploadFile("falguni/package.json","/Users/bhavi/Desktop/vercel/package.json");
+// import { createClient } from "redis";
+// const publisher = createClient();
+// publisher.connect();
+
+
+
+// const app = express();
+
+// app.use(cors())
+// app.use(express.json());
+// console.log(path.join(__dirname,`output/randomstring`))
+
+// //POSTMAN
+// app.post("/deploy",async (req,res) => {
+//     const repoUrl = req.body.repoUrl;
+//     const id = generate(); //asd12
+//     await simpleGit().clone(repoUrl, path.join(__dirname,`output/${id}`));
+
+//     const files = getAllFiles(path.join(__dirname,`output/${id}`));
+
+//     files.forEach(async file => {
+
+//         //from: //users/bhavi/vercel.....//output//13125512/app.js
+//         //extract : //output/13125512/app.js
+//         await uploadFile(file.slice(__dirname.length + 1),file);    //users/bhavi/vercel...../output/.. (yeh reemove ho jayega )
+//     })
+
+//     publisher.lPush("build-queue",id);
+
+//     // console.log(files);
+//     // files.forEach(file=>{
+//     //     S3.upload
+//     // })
+
+//     //put this to s3
+//     res.json({
+//         id:id
+//     })
+// });
+
+//     //aws-sdk
+//     //sdk.uploadDirToS3()=> no easy way to call this
+//     //need to make an array
+//     // iterate thru this array one by array
+//     //[/falz/users/project/vercel/output/12345/app.tsx]
+//     //then call sdk.uploadFile one by one cz there isnt an easy way to upload sdk.folder
+
+
+
+// app.listen(3000);
+
+// // function getAllFiles(arg0: any) {
+// //     throw new Error("Function not implemented.");
+// // }
 
